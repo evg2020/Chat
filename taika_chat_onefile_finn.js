c930477abf8c7f6e88c4ddffda126193be8c9ec5
Evgeny Fedorenko <evgeny.fedorenko@constant.obninsk.ru>
2022-05-30 11:06:16 +0300

    feat : change time delay to 1400ms

--- taika_chat_onefile_finn.js
+++ taika_chat_onefile_finn.js
@@ -1896,7 +1896,7 @@ var EmbedTaikaChatUI = EmbedTaikaChatUI || (function () {
           setTimeout(function(){
             $('#taika-chat-messages').append(`<div class='taika-agent-message' ><div class="message">Saimme viestinne. Että
             asiakaspalvelun edustaja liittyy pian.</div></div>`);
-          }, 1250)
+          }, 1400)
         }
         this.chatMessagesScrollTop();
         this.checkTextLimit();
